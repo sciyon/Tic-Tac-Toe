9073754c1711bb037838eec6d1db2b159f798b82
}{alt <ryonic.ccy@gmail.com>
2023-03-11 11:03:24 +0800

    Add project files.

---
+++ TicTacToe/TicTacToe/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TicTacToe.ViewModels;
+using Xamarin.Forms;
+
+namespace TicTacToe.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using TicTacToe.Models;
+using TicTacToe.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TicTacToe.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
